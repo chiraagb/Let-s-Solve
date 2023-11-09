9ca1a88d1c8104bb0d71319a45c3a79641c25a58
Chirag Bhandakkar <chiraagbhandakkar@gmail.com>
2023-11-09 13:39:37 +0530

    changed

--- src/components/RootLayout.jsx
+++ src/components/RootLayout.jsx
@@ -27,7 +27,7 @@ const Header = ({
       <div className="flex items-center justify-between">
         {/* Logo */}
         <Link href={"/"} aria-label="Home">
-          <Logo invert={invert}>Let’s Solve</Logo>
+          <Logo invert={invert}>Lets Solve</Logo>
         </Link>
         <div className="flex items-center gap-x-8">
           <Button href={"/contact"} invert={invert}>
